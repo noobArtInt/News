0ffe2d08c0d4aef3a8942902b02647f7d3d7edcf
Anuj Karn <61925653+noobArtInt@users.noreply.github.com>
2020-12-12 18:48:53 +0530

    Added Shorts News

--- src/DataFetching.js
+++ src/DataFetching.js
@@ -12,27 +12,73 @@ class DataFetching extends React.Component {
           search
           onChange={this.handleChange}
         />
+        <h2> Shorts </h2>
+        <div>
+          <button className="card">
+            <div className="container">
+              <p>
+                <strong>News 1</strong>
+              </p>
+              bla bla bla
+              <div>
+                <button className="card">
+                  <div className="container">
+                    <p>Sector 1 </p>
+                  </div>
+                </button>
+                <button className="card">
+                  <div className="container">
+                    <p>Sector 2</p>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </button>
+          <button className="card">
+            <div className="container">
+              <p>
+                <strong>News 2</strong>
+              </p>
+              bla bla bla
+              <div>
+                <button className="card">
+                  <div className="container">
+                    <p>Sector Up </p>
+                  </div>
+                </button>
+                <button className="card">
+                  <div className="container">
+                    <p>Sector Down</p>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </button>
+        </div>
 
-        <button className="card">
-          <div className="container">
-            <p>IT</p>
-          </div>
-        </button>
-        <button className="card">
-          <div className="container">
-            <p>Automobile</p>
-          </div>
-        </button>
-        <button className="card">
-          <div className="container">
-            <p>Finance</p>
-          </div>
-        </button>
-        <button className="card">
-          <div className="container">
-            <p>Pharmacy</p>
-          </div>
-        </button>
+        <div className="Sector">
+          <h2>Sectors</h2>
+          <button className="card">
+            <div className="container">
+              <p>IT</p>
+            </div>
+          </button>
+          <button className="card">
+            <div className="container">
+              <p>Automobile</p>
+            </div>
+          </button>
+          <button className="card">
+            <div className="container">
+              <p>Finance</p>
+            </div>
+          </button>
+          <button className="card">
+            <div className="container">
+              <p>Pharmacy</p>
+            </div>
+          </button>
+        </div>
       </div>
     );
   }
